2ac1d8320c193cc4f8e5789c958ba3b2e1528867
David Stuber <rlzdavid20@gmail.com>
2024-03-10 16:15:51 +0100

    Test PR

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -15,7 +15,7 @@ class EventController extends Controller
     public function index()
     {
         return EventResource::collection(
-            Event::with('user')->paginate() // test-2
+            Event::with('user')->paginate() // test-3
         );
     }
 
