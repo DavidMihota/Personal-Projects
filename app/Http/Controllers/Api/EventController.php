6fc6f976f3cf412e76607b59c7c36c17f967cf7b
David Stuber <rlzdavid20@gmail.com>
2024-06-01 21:41:37 +0200

    Restricted routes for Create, Destroy and Update events

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -11,6 +11,11 @@ use Illuminate\Http\Request;
 class EventController extends Controller
 {
     use CanLoadRelationships;
+
+    public function __construct()
+    {
+        $this->middleware("auth:sanctum")->except(['index', 'show']);
+    }
     private array $relations = ['user', 'attendees', 'attendees.user'];
     /**
      * Display a listing of the resource.
@@ -36,7 +41,7 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time'
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
 
         return new EventResource($this->loadRelationships(($event)));
