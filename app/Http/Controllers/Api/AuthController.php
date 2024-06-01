c50d3d4a99120f060f47e53ddc6e4a34ee43d372
David Stuber <rlzdavid20@gmail.com>
2024-06-01 22:02:15 +0200

    API Logout System (token remove)

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -41,6 +41,10 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
+        $request->user()->currentAccessToken()->delete();
 
+        return response()->json([
+            'message' => "You was logged out successfully!"
+        ]);
     }
 }
