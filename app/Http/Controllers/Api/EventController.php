f89952a057195b4ef6d3cc45f63e94e162000f2a
David Stuber <rlzdavid20@gmail.com>
2024-06-05 16:45:11 +0200

    Added Gate-Authorization for event update and attendee delete

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -7,6 +7,7 @@ use App\Http\Traits\CanLoadRelationships;
 use \App\Models\Event;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class EventController extends Controller
 {
@@ -61,6 +62,8 @@ class EventController extends Controller
      */
     public function update(Request $request, Event $event)
     {
+        $this->authorize('update-event', $event);
+
         $event->update(
             $request->validate([
                 'name' => 'sometimes|string|max:255',
